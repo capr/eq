f68883a987fb4e0aa12e4b2ddb8a2c73656deb98
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-20 21:48:02 +0200

    eq2 & eq3 merge

--- eq_test.lua
+++ eq_test.lua
@@ -1,12 +1,31 @@
-local solve_equation3 = require'eq3'
+local eq = require'eq'
 
 local pformat = require'pp'.pformat
+
 local function assert_approx(a,b)
 	print(string.format("approximation error:\t%g",math.abs(a-b)))
 	assert(math.abs(a-b) <= 1e-14, a..' ~= '..b)
 end
+
+local function assert_sol(a, b, c, s1, s2)
+	local t = {eq.solve2(a, b, c)}
+	local s = {s1, s2}
+	assert(#t == #s, pformat(t))
+	table.sort(t)
+	table.sort(s)
+	for i=1,#t do
+		assert_approx(t[i],s[i])
+	end
+end
+assert_sol(0, 0, 1       )   --degree 1, c ~= 0
+assert_sol(0, 1, -2,    2)   --degree 1, -c/b
+assert_sol(1, -1, 2      )   --D < 0
+assert_sol(1, 2, 1,    -1)   --D > 0
+assert_sol(2, 2, 0, -1, 0)   --D == 0
+
+
 local function assert_sol(a, b, c, d, s1, s2, s3)
-	local t = {solve_equation3(a, b, c, d)}
+	local t = {eq.solve3(a, b, c, d)}
 	local s = {s1, s2, s3}
 	assert(#t == #s, pformat(t))
 	table.sort(t)
@@ -15,7 +34,6 @@ local function assert_sol(a, b, c, d, s1, s2, s3)
 		assert_approx(t[i],s[i])
 	end
 end
-
 assert_sol(1, -4, -9, 36,    4, -3, 3) --D < 0
 assert_sol(1, -6, 11, -6,    1, 2, 3) --D < 0
 assert_sol(1, -3,  3, -1,    1) --D == 0, u == 0
@@ -29,4 +47,3 @@ assert_sol(0,  1, -7, 12,    3, 4) --2nd degree
 assert_sol(2, -4, -22, 24,   4, -3, 1)
 assert_sol(2, -4, -22, 24,   4, -3, 1)
 assert_sol(4, -3, -25, -6,   3, -1/4, -2)
-
